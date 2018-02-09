f3c7118877efa27dbde390ec0f18a0dc865793db
WildCode <cadainmiller@gmail.com>
2018-02-08 21:44:32 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,38 +4,6 @@ namespace DotNet____Vivian
 {
     class Program
     {
-        public string name ;
-        public  string address; 
-        public int phonenumber;
-        public string ptype;
-        public double rfinalcost;
-
-        static void Main(string[] args)
-        {
-            Gold Gnewmember;
-            Gnewmember = new Gold();
-
-            Regular Rnewmember;
-            Rnewmember = new Regular();
-
-            int choice =0 ;
-
-            DisplayMenu();
-            choice =  Convert.ToInt32(Console.ReadLine());
-
-            if (choice == 1)
-            {
-                Guest();
-            }
-            if (choice == 2)
-            {
-                Console.WriteLine("2");
-            }
-            if (choice != 1 || choice != 2)
-            {
-                Console.WriteLine("The variable is set to false.");
-            }
-        }
 
         static void DisplayMenu()
         {
@@ -45,34 +13,6 @@ namespace DotNet____Vivian
             Console.WriteLine("3.Exit\n");
             Console.WriteLine();
             Console.WriteLine("Enter your choice:");
-        }
-
-         static void Guest()
-        {
-            Regular Rnewmember;
-            Rnewmember = new Regular();
-
-            Console.Clear();
-            // Input from user about name
-            Console.WriteLine("Enter your name:");
-            Rnewmember.name = Convert.ToString(Console.ReadLine());
-            // Input from user about address
-            Console.WriteLine("Enter your address:");
-            Rnewmember.address = Convert.ToString(Console.ReadLine());
-            // Input from user about telephone
-            Console.WriteLine("Enter your telephone number:");
-            Rnewmember.telephone = Convert.ToInt32(Console.ReadLine());
-            // Input from user about Payment 
-            Console.WriteLine("Enter your Payment method:");
-            Rnewmember.PaymentM = Convert.ToString(Console.ReadLine());
-            Package();
-
-            Console.WriteLine("Name: {0}");
-            Console.WriteLine("Address: {0}");
-            Console.WriteLine("Telephone: {0}");
-            Console.WriteLine("Payment Method: {0}");
-            Console.WriteLine("Shipment Cost: {0}");
-
         }
 
         static void Package () // Package Function 
@@ -113,96 +53,136 @@ namespace DotNet____Vivian
             Console.WriteLine("3 >>> Same-Day 6 - 12 hours || Per Lbs between 0 -10 || Every extra Lbs ");
 
             servicetype = Convert.ToInt32(Console.ReadLine());
-    
-
-            if ( servicetype == 1)
+            while (servicetype!=4)
             {
-                if (weight > 10 )
+            switch (servicetype)
                 {
-                    minusweight = weight - standardweight;
-                    cost = minusweight*standardcostextra;
-                    cost1 = standardcost*standardweight;
+                    case 1:
+                        if (weight > 10 )
+                        {
+                            minusweight = weight - standardweight;
+                            cost = minusweight*standardcostextra;
+                            cost1 = standardcost*standardweight;
 
-                    finalcost = cost1+cost;
-                    Console.ReadKey();
-                    RegularReceipt();
+                            finalcost = cost1+cost;
+                            Console.WriteLine("Shipment {0}", finalcost);
 
+                        }
+                        else 
+                        {
+
+                        finalcost = weight*standardcost;
+
+                        }
+                        break;
+
+                        case 2:
+
+                        if (weight > 10 )
+                        {
+                            minusweight = weight - standardweight;
+                            cost = minusweight*overnightcostextra;
+                            cost1 = overnightcost*standardweight;
+
+                            finalcost = cost1+cost;
+
+                        }
+                        else 
+                        {
+                            
+                        finalcost = weight*overnightcost;
+
+                        }
+                        break;
+
+                        case 3:
+
+                         if (weight > 10 )
+                            {
+                                minusweight = weight - standardweight;
+                                cost = minusweight*samedaycostextra;
+                                cost1 = samedaycost*standardweight;
+
+                                finalcost = cost1+cost;
+
+                            }
+                            else 
+                            {
+                                
+                            finalcost = weight*samedaycost;
+
+                            }
+                        break;
+                    default:
+                    {
+                        Console.WriteLine("Nothing nuh deh ya");
+                        break;
+                    }
                 }
-                else 
-                {
-
-                 finalcost = weight*standardcost;
-                 Console.ReadKey();
-
-                }
-
-            }
-            if ( servicetype == 2)
-            {
-                if (weight > 10 )
-                {
-                    minusweight = weight - standardweight;
-                    cost = minusweight*overnightcostextra;
-                    cost1 = overnightcost*standardweight;
-
-                    finalcost = cost1+cost;
-                    Console.ReadKey();
-
-                }
-                else 
-                {
-                    
-                 finalcost = weight*overnightcost;
-                 Console.ReadKey();
-
-                }
-            }
-
-            if ( servicetype == 3)
-            {
-                if (weight > 10 )
-                {
-                    minusweight = weight - standardweight;
-                    cost = minusweight*samedaycostextra;
-                    cost1 = samedaycost*standardweight;
-
-                    finalcost = cost1+cost;
-                    Console.ReadKey();
-
-                }
-                else 
-                {
-                    
-                 finalcost = weight*samedaycost;
-                 Console.ReadKey();
-
-                }
-            }
-            else
-            {
-                Console.WriteLine("Pleaase Enter one of these choise 1,2 or 3 ");
-                Console.ReadKey();
             }
 
             
         }
 
-            static void PackageCost()
+         static void Guest()
+        {
+            Regular Rnewmember;
+            Rnewmember = new Regular();
 
+            Console.Clear();
+            // Input from user about name
+            Console.WriteLine("Enter your name:");
+            Rnewmember.name = Convert.ToString(Console.ReadLine());
+            // Input from user about address
+            Console.WriteLine("Enter your address:");
+            Rnewmember.address = Convert.ToString(Console.ReadLine());
+            // Input from user about telephone
+            Console.WriteLine("Enter your telephone number:");
+            Rnewmember.telephone = Convert.ToInt32(Console.ReadLine());
+            // Input from user about Payment 
+            Console.WriteLine("Enter your Payment method:");
+            Rnewmember.PaymentM = Convert.ToString(Console.ReadLine());
+
+            Package();
+
+            Console.Clear();
+
+           Console.WriteLine("Name: {0}", Rnewmember.name);
+           Console.WriteLine("Address: {0}", Rnewmember.address);
+           Console.WriteLine("Telephone: {0}", Rnewmember.telephone);
+           Console.WriteLine("Payment Method: {0}", Rnewmember.PaymentM);
+           Console.WriteLine("Shipment Cost: {0}");
+
+           // Console.ReadKey();
+
+        }
+    static void Main(string[] args)
             {
+                Gold Gnewmember;
+                Gnewmember = new Gold();
 
+                Regular Rnewmember;
+                Rnewmember = new Regular();
+
+                int choice =0 ;
+
+                DisplayMenu();
+                choice =  Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Guest();
+                }
+                if (choice == 2)
+                {
+                    Console.WriteLine("2");
+                }
+                if (choice != 1 || choice != 2)
+                {
+                    Console.WriteLine("The variable is set to false.");
+                }
             }
 
-            static void RegularReceipt()
-            {
-                Console.WriteLine("Name: {0}");
-                Console.WriteLine("Address: {0}");
-                Console.WriteLine("Telephone: {0}");
-                Console.WriteLine("Payment Method: {0}");
-                Console.WriteLine("Shipment Cost: {0}");
-
-
-            }
 
     }
 }
